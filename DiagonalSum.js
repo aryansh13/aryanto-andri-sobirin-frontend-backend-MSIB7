3b3c196fe99af34c2c5cf25fab75ba692192275c
aryaas_ <akunto21@gmail.com>
2024-08-25 08:34:53 +0700

    first commit

--- DiagonalSum.js
+++ DiagonalSum.js
@@ -1,34 +1,33 @@
-function hitungJumlahDiagonal(matriks) {
-  let total = 0;
-  let ukuran = matriks.length;
-
-  for (let i = 0; i < ukuran; i++) {
-    // Diagonal utama
-    total += matriks[i][i];
-
-    // Diagonal sekunder
-    let j = ukuran - 1 - i;
-    if (i != j) {
-      total += matriks[i][j];
+function hitungDiagonal(m) {
+  let t = 0;
+  let p = m.length;
+  
+  for (let i = 0; i < p; i = i + 1) {
+    t = t + m[i][i];
+    
+    let k = p - 1 - i;
+    if (i != k) {
+      t = t + m[i][k];
     }
   }
-
-  return total;
+  
+  return t;
 }
 
-// Tes fungsi
-let matriks1 = [
+// Contoh penggunaan
+let m1 = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9],
 ];
 
-let matriks2 = [
+let m2 = [
   [1, 2, 3, 4],
   [5, 6, 7, 8],
   [9, 10, 11, 12],
   [13, 14, 15, 16],
 ];
 
-console.log("Hasil matriks 3x3:", hitungJumlahDiagonal(matriks1)); // Hasil 25
-console.log("Hasil matriks 4x4:", hitungJumlahDiagonal(matriks2));// Hasil 68
+// Tampilkan hasil
+console.log("Hasil Matrik 1 :  ", hitungDiagonal(m1)); //Hasil 25
+console.log("Hasil Matrik 2 :  ",hitungDiagonal(m2)); //Hasil 68
